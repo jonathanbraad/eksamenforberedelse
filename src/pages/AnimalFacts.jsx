88a6f9bbbb0d3e918b6ddac8f5b81cebb189b736
Alexander <Alexanderhusig1@gmail.com>
2022-11-27 16:03:13 +0100

    Added searchbar to recipes

--- src/pages/AnimalFacts.jsx
+++ src/pages/AnimalFacts.jsx
@@ -14,10 +14,9 @@ function AnimalFacts() {
 
     return (
         <div className="divplacement">
-            <h3>Animal Facts</h3>
-            <p>Cat fact: {facts.catFact}</p>
-            <p>Dog fact: {facts.dogFact}</p>
-            <button onClick={getFacts} type="submit">Get new facts</button>          
+            <h3>Recipes</h3>
+            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search by ingredient"></input>
+            <button onClick={getFacts} type="submit">Search</button>          
         </div>
     );
 }
